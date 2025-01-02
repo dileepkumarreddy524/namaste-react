d233c9dd59a8e86a5d4b38b92c404dd6d488e0e6
vr_lingampalli.d <vr_lingampalli.d@capitaland.com>
2025-01-02 17:47:28 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const parent = React.createElement('div',{id: 'parent'},
     React.createElement('div',{id: 'child'}, 
-    [React.createElement('h1',{id: 'heading'},'Dileep kumar reddy frontend dev'),React.createElement('h1',{id: 'heading2'},'Senior software developer')])
+    [React.createElement('h1',{id: 'heading'},'Dileep kumar reddy frontend dev'),React.createElement('h1',{id: 'heading2'},'Senior software developer at valtech')])
 );
-
-
-//const heading = React.createElement('h1',{id: 'heading'},'Dileep kumar frontend dev');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
