61b3b09dda218f24fd9de0fabd3c3e8a1af01df1
vr_lingampalli.d <vr_lingampalli.d@capitaland.com>
2025-01-02 19:57:48 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-const parent = React.createElement('div',{id: 'parent'},
-    React.createElement('div',{id: 'child'}, 
-    [React.createElement('h1',{id: 'heading'},'Dileep kumar reddy frontend dev'),React.createElement('h1',{id: 'heading2'},'Senior software developer at valtech')])
+
+//JSX
+//React Element
+const designation = <h2 className="heading">Senior Software Developer</h2>;
+const experience = "6 years"
+//React Component
+//Class Based components - OLD
+//Functional components - NEW
+//Functional component
+const Title = () => <h1 className="heading">React Application</h1>;
+const HeadingComponent = () => (
+    <div id="container">
+        <Title/>
+        <h1 className="heading">Dileep Kumar Reddy L</h1>
+        { designation }
+        <h2>{ experience }</h2>
+        <h3>{ console.log("Test") }</h3>
+        { Title() }
+    </div>
 );
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
